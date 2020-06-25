20f0b1b718e27011e1538497138aa9d207099f8c
gal <tanjhu@163.com>
2020-06-25 16:51:15 +0800

    定制model的字段

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from .fields import  OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -21,7 +22,7 @@ class Course(models.Model):
                               on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject,
                                 related_name='courses',
-                                on_delete=models.CASCADE)
+                                on_delete=models.CASCADE,null=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
@@ -40,14 +41,18 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
         return f'{self.order}. {self.title}'
 
+    class Meta:
+        ordering = ['order']
+
 class Content(models.Model):
     module = models.ForeignKey(Module,
                                                                     related_name='contents',
-                                                                     on_delete=models.CASCADE)
+                                                                    on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,
                                                                               on_delete=models.CASCADE,
                                                                                limit_choices_to={'model__in':(
@@ -57,6 +62,10 @@ class Content(models.Model):
                                                                                'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
